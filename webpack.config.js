cc8506533d2e5996b29d411de08c6fc72fb25aa0
stevenacalhoun <stevenacalhoun@gmail.com>
2016-08-31 11:20:08 -0400

    Webpack fix for CNAME copy

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
       }),
       new CopyWebpackPlugin([
         { from: 'app/files/Steven\ Calhoun\ Resume.pdf', to: 'files/Steven\ Calhoun\ Resume.pdf' },
-        { from: 'CNAME', to: 'CNAME'},
+        { from: 'CNAME', to: './'}
       ])
     ]
 };
